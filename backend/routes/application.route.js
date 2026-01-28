90ab49cf56eb8c603624f5aa0c6fac27d4c72ccb
Pushkar Vyas <vyaspushkar18@gmail.com>
2026-01-28 19:46:19 +0530

    Add application controller and routes

---
+++ backend/routes/application.route.js
@@ -0,0 +1,13 @@
+import express  from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import router from "./user.route.js";
+import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../controllers/application.controller.js";
+
+router.route("/apply/:id").get(isAuthenticated, applyJob);
+router.route("/get").get(isAuthenticated, getAppliedJobs);
+router.route("/:id/applicants").get(isAuthenticated, getApplicants);
+router.route("/status/:id/update").post(isAuthenticated, updateStatus);
+
+
+
+export default router;
