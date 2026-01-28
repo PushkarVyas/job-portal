dc46d2417bf7ed1e450cb89737875b36965f6548
Pushkar Vyas <vyaspushkar18@gmail.com>
2026-01-28 13:37:38 +0530

    Added MongoDB models for users, jobs, companies, and applications

---
+++ backend/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: Number,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["student", "recruiter"],
+        default: "student"
+    },
+    profile:{
+        bio:{
+            type: String,
+            required: false
+        },
+        skills: [{type: String}],
+        resume: {type: String},
+        resumeOriginalName: {type: String},
+        company: {type: mongoose.Schema.Types.ObjectId, ref: "Company"},  
+        profilephoto: {type: String, default: ""}
+    },
+} , {timestamps: true});
+
+export const User = mongoose.model("User", userSchema);
