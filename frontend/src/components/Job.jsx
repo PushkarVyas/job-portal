9da5e283558f4bee3e42a4f0641e84e5f8fd714d
Pushkar Vyas <vyaspushkar18@gmail.com>
2026-02-05 09:52:47 +0530

    getting jobs dynamically

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -6,13 +6,21 @@ import { AvatarImage } from "@radix-ui/react-avatar";
 import { Badge } from "./ui/badge";
 import { useNavigate } from "react-router-dom";
 
-function Job() {
+function Job({job}) {
   const navigate = useNavigate();
-  const jobId = 'fjdpwcdu';
+  
+  const dayAgoFunction = (mongodbTime) => {
+    const createdAt = new Date(mongodbTime);
+    const currentTime = new Date();
+    const timeDifference = currentTime.getTime() - createdAt.getTime();
+    return Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  }
+
+
   return (
     <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100">
       <div className="flex item-center justify-between">
-        <p className="text-sm text-gray-500">2 days ago</p>
+        <p className="text-sm text-gray-500">{(job?.createdAt) === 0 ? "Today" : `${dayAgoFunction(job?.createdAt)} days ago`}</p>
         <Button variant="outline" className="rounded-full" size="icon">
           <Bookmark />
         </Button>
@@ -25,32 +33,30 @@ function Job() {
           </Avatar>
         </Button>
         <div>
-          <h1 className="font-medium text-lg ">Company name</h1>
-          <p className="text-sm text-gray-500">India</p>
+          <h1 className="font-medium text-lg ">{job?.company?.name}</h1>
+          <p className="text-sm text-gray-500">{job?.company?.location}</p>
         </div>
       </div>
 
       <div>
-        <h1 className="font-bold text-lg my-2">Title</h1>
+        <h1 className="font-bold text-lg my-2">{job?.title}</h1>
         <p className="text-sm text-gray-600">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi,
-          reprehenderit! Cupiditate accusamus sequi, magni ullam excepturi eius
-          magnam illum quis?
+            {job?.description}
         </p>
       </div>
       <div className="flex gap-2 items-center mt-4">
         <Badge className={"text-blue-700 font-bold"} variant="ghost">
-          12 Positions
+          {job?.position} Positions
         </Badge>
         <Badge className={"text-[#F83002] font-bold"} variant="ghost">
-          Part time
+          {job?.type}
         </Badge>
         <Badge className={"text-[#7209b7] font-bold"} variant="ghost">
-          24 LPA
+          {job?.salary} LPA
         </Badge>
       </div>
       <div className="flex items-center gap-4 mt-4">
-        <Button onClick={() => navigate("/description/${jobId}")} variant="outline">Details</Button>
+        <Button onClick={() => navigate("/description/${job?._id}")} variant="outline">Details</Button>
         <Button className="bg-[#7209b7]">Save for later</Button>
       </div>
     </div>
--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import LatestJobCards from "./LatestJobCards";
+import { useSelector } from "react-redux";
 
-const randomJobs = [1, 2, 3, 4, 5, 6, 7, 8];
+
 
 const LatestJobs = () => {
+
+  const {allJobs} = useSelector(store => store.job);
+
   return (
     <div className="max-w-7xl mx-auto my-20">
       <h1 className="text-4xl font-bold">
@@ -12,8 +16,8 @@ const LatestJobs = () => {
       </h1>
 
       <div className="grid grid-cols-3 gap-4 my-5">
-        {randomJobs.slice(0, 6).map((item, index) => (
-          <LatestJobCards key={index} />
+        { allJobs.length <= 0 ? <span>No job available</span> : allJobs.slice(0, 6).map((job) => (
+          <LatestJobCards key={job._id} job={job} />
         ))}
       </div>
     </div>
