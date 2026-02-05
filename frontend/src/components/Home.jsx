6ce732561c04483a8f8d9c490a36b61b107b4594
Pushkar Vyas <vyaspushkar18@gmail.com>
2026-02-05 13:37:38 +0530

    Add admin company flow and company redux state

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './shared/Navbar'
 
 import HeroSection from './HeroSection'
@@ -6,9 +6,18 @@ import CategoryCarousel from './CategoryCarousel'
 import LatestJobs from './LatestJobs'
 import Footer from './shared/Footer'
 import useGetAllJobs from '@/hooks/useGetAllJobs'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
   useGetAllJobs();
+  const user = useSelector(store => store.auth.user);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(user?.role === 'recruiter'){
+      navigate("/admin/companies")
+    }
+  }, [user])
   return (
     <div>
       <Navbar/>
